547bee9a5a37e81ff3de9772b701e1213bc8985f
laxy <laxyrajput@gmail.com>
2024-08-08 09:57:07 +0530

    adding signup form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './Header'
 
 const Login = () => {
+
+    const [isSignInForm, setIsSignInForm] = useState(true);
+
+    const toggleSignInForm = () => {
+        setIsSignInForm(!isSignInForm)
+    }
+
     return (
         <div>
 
@@ -19,24 +26,40 @@ const Login = () => {
             <div>
                 <form className='w-3/12 absolute p-12 bg-opacity-90 bg-neutral-900 my-36 mx-auto right-0 left-0 text-white'>
 
-                    <h1 className='font-bold text-3xl py-4'>Sign In</h1>
+                    <h1 className='font-bold text-3xl py-4'>
+                        {isSignInForm ? "Sign In" : "Sign Up"}
+                    </h1>
 
-                    <input
+                    {isSignInForm && (<input
                         type='text'
                         placeholder='Email or mobile number'
                         className='p-4 my-4 w-full bg-opacity-90 bg-neutral-900 border-solid rounded-md border-red-900'
-                    />
+                    />)}
+
+                    {!isSignInForm && (<input
+                        type='text'
+                        placeholder='Email'
+                        className='p-4 my-4 w-full bg-opacity-90 bg-neutral-900 border-solid rounded-md border-red-900'
+                    />)}
+
+                    {!isSignInForm && (<input
+                        type='text'
+                        placeholder='Mobile number'
+                        className='p-4 my-4 w-full bg-opacity-90 bg-neutral-900 border-solid rounded-md border-red-900'
+                    />)}
+                    
                     <input
                         type='password'
                         placeholder='Password'
                         className='p-4 my-4 w-full opacity-90 bg-neutral-900 border-solid rounded-md border-red-900'
                     />
-
                     <button className='p-4 my-4 bg-red-700 w-full rounded-md'>
-                        Sign In
+                        {isSignInForm ? "Sign In" : "Sign Up"}
                     </button>
-                    <p className='py-4'>
-                        New to Netflix? Sign up now.
+
+                    <p className='py-4 cursor-pointer' onClick={toggleSignInForm}>
+                        {isSignInForm ? "New to Netflix? Sign up now." : "Already a user? Sign in now"}
+
                     </p>
                 </form>
             </div>
