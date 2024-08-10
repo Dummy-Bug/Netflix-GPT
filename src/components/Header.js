dd3fa2918a46ab860435c34595151cdcfda053fe
laxy <laxyrajput@gmail.com>
2024-08-10 12:39:38 +0530

    added hardcorded values

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,7 @@ import { useSelector, useDispatch } from "react-redux";
 
 import { useEffect } from "react";
 import { addUser, removeUser } from "../utils/userSlice";
+import { LOGO } from '../utils/constants';
 
 const Header = () => {
 
@@ -55,7 +56,7 @@ const Header = () => {
 
             <img
                 className='w-44'
-                src='https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png'
+                src={LOGO}
                 alt="logo">
             </img>
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -12,6 +12,8 @@ import { auth } from "../utils/firebase";
 import { addUser } from "../utils/userSlice";
 import { useNavigate } from 'react-router-dom';
 
+import { USER_AVATAR } from '../utils/constants';
+
 const Login = () => {
 
     const [isSignInForm, setIsSignInForm] = useState(true);
@@ -47,7 +49,7 @@ const Login = () => {
                 const user = userCredential.user;
                 updateProfile(user, {
                     displayName: name.current.value,
-                    photoURL: "https://occ-0-2611-3662.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABTZ2zlLdBVC05fsd2YQAR43J6vB1NAUBOOrxt7oaFATxMhtdzlNZ846H3D8TZzooe2-FT853YVYs8p001KVFYopWi4D4NXM.png?r=229"
+                    photoURL: USER_AVATAR
                 });
             }).then(() => {
                 const { uid, email, displayName, photoURL } = auth.currentUser;
